45a86fa2eddf1614363a1766fa7392115508dfb5
n6b2b <jwong32@alumni.ubc.ca>
2019-05-01 09:34:31 -0700

    heat map of cherry blossoms in vancouver

---
+++ graph.py
@@ -0,0 +1,36 @@
+import matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from gmplot import gmplot
+
+vancouver_file_path = "/Users/jonathan/Downloads/csv_street_trees/StreetTrees_CityWide.csv"
+
+vancouver_data = pd.read_csv(vancouver_file_path)
+
+vancouver_data_cn = vancouver_data.set_index("COMMON_NAME", drop=False)
+
+# filter all cherry blossom trees
+cherry_blossom = vancouver_data_cn.loc["KWANZAN FLOWERING CHERRY", :]
+
+# extracting latitude and longitude
+cherry_blossom_location = cherry_blossom[["LATITUDE", "LONGITUDE"]]
+
+# removing all trees without location
+cherry_blossom_location_final = cherry_blossom_location.loc[cherry_blossom_location["LATITUDE"] > 0]
+
+locations = cherry_blossom_location_final.to_numpy()
+
+lats = []
+longs = []
+for l in locations:
+    lats.append(l[0])
+    longs.append(l[1])
+
+# center location near QE park
+gmap = gmplot.GoogleMapPlotter(49.238680, -123.139569, 13)
+
+gmap.heatmap(lats, longs)
+
+gmap.draw("cherry_blossoms.html")
+
